9869f40a496e4fd27b2d12e16ce099644d33a0de
dancey-apple <dancey.apple@proton.me>
2024-03-21 14:18:11 -0500

    if-statements-for-add-to-cart

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let unArr;
 let avatar;
 const loginButton = document.getElementById('login-button');
 
-loginButton.onclick = logInUser
+loginButton.onclick = logInUser;
 
 function logInUser(){
     username = prompt("What's your first and last name?");
@@ -30,31 +30,89 @@ function displayAvatar() {
     loginButton.style.display = 'none';
     document.getElementById('avatar').innerText = avatar;
 }
-
-//--Shopping Cart--
+console.log('ready for you to push a button')
+//---------------------Shopping Cart----------------------
 const cart = [];
+const items = [
+    {
+        name: "quarter",
+        price: 500
+    },
+    {
+        name: "half",
+        price: 900
+    },
+    {
+        name: "whole",
+        price: 1500
+    },
+    {
+        name: "steak",
+        price: 12.00
+    }, 
+    {
+        name: "roast",
+        price: 8.00
+    }, 
+    {
+        name: "ground beef",
+        price: 5.00
+    }, 
+    {
+        name: "jerky",
+        price: 15.00
+    }
 
-
-// Shopping Cart Icon - Button
-const addToCart = document.getElementById('add-quarter');
+];  
+const addToCart = document.getElementsByClassName('add-to-cart');
+const buttonID = document.getElementById("add-*");
 addToCart.onclick = addItemToCart;
 
 // Add to cart button(s)
 function addItemToCart() {
-    const item = {
-        name: "quarter",
-        price: 500
+    let item;
+    if (buttonID === "quarter") {   
+        item = items[0];
+        cart.push(item);
     }
-    cart.push(item);
-    //updateCartList();
-    //updateCartTotal();
+    if (buttonID === "half") {   
+        item = items[1];
+        cart.push(item);
+        console.log(cart);
+    }
+    if (buttonID === "whole") {   
+        item = items[2];
+        cart.push(item);
+        console.log(cart);
+    }
+    if (buttonID === "steak") {   
+        item = items[3];
+        cart.push(item);
+        console.log(cart);
+    }
+    if (buttonID === "roast") {   
+        item = items[4];
+        cart.push(item);
+        console.log(cart);
+    }
+    if (buttonID === "ground beef") {   
+        item = items[5];
+        cart.push(item);
+        console.log(cart);
+    }
+    if (buttonID === "jerky") {   
+        item = items[6];
+        cart.push(item);
+        console.log(cart);
+    }
+    else {
+        console.log("Error: Item not found");
+    }
     console.log(cart);
 }
 
 
 // # of items icon
-
-// Add to cart button(s)
 
 // Update Cart List
 function updateCartList(){}
